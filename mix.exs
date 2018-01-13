150a160ab2022231f86ebbd52bcb12123e10fd8c
Dunya Kirkali <dunyakirkali@gmail.com>
2018-01-13 21:18:00 +0100

    Set preferred cli env

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule SunTimes.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      preferred_cli_env: [espec: :test],
     ]
   end
 
