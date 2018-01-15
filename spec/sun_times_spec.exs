216e0f0947d3cbd3a9478fb49394f054930c770d
Dunya Kirkali <dunyakirkali@gmail.com>
2018-01-15 21:20:21 +0100

    Dry up timzone params

--- spec/sun_times_spec.exs
+++ spec/sun_times_spec.exs
@@ -9,50 +9,51 @@ defmodule SunTimesSpec do
   describe "Amsterdam" do
     let :lat, do: 52.3702
     let :lon, do: 4.8952
+    let :timezone, do: "Europe/Amsterdam"
 
     describe "on Jan 14th 2018" do
-      let :day, do: Timex.to_datetime({2018 , 1, 14}, "Europe/Amsterdam")
+      let :day, do: Timex.to_datetime({2018 , 1, 14}, timezone())
 
       it "rise" do
-        rise = Timex.to_datetime({{2018, 1, 14}, {8, 44, 00}}, "Europe/Amsterdam")
+        rise = Timex.to_datetime({{2018, 1, 14}, {8, 44, 00}}, timezone())
 
         expect(SunTimes.rise(day(), lat(), lon())).to be_close_to(rise, tolerance())
       end
 
       it "set" do
-        set = Timex.to_datetime({{2018, 1, 14}, {16, 55, 00}}, "Europe/Amsterdam")
+        set = Timex.to_datetime({{2018, 1, 14}, {16, 55, 00}}, timezone())
 
         expect(SunTimes.set(day(), lat(), lon())).to be_close_to(set, tolerance())
       end
     end
 
     describe "on Jan 30th 2017" do
-      let :day, do: Timex.to_datetime({2017 , 1, 30}, "Europe/Amsterdam")
+      let :day, do: Timex.to_datetime({2017 , 1, 30}, timezone())
 
       it "rise" do
-        rise = Timex.to_datetime({{2017, 1, 30}, {8, 24, 00}}, "Europe/Amsterdam")
+        rise = Timex.to_datetime({{2017, 1, 30}, {8, 24, 00}}, timezone())
 
         expect(SunTimes.rise(day(), lat(), lon())).to be_close_to(rise, tolerance())
       end
 
       it "set" do
-        set = Timex.to_datetime({{2017, 1, 30}, {17, 23, 00}}, "Europe/Amsterdam")
+        set = Timex.to_datetime({{2017, 1, 30}, {17, 23, 00}}, timezone())
 
         expect(SunTimes.set(day(), lat(), lon())).to be_close_to(set, tolerance())
       end
     end
 
     describe "on May 11th 1984" do
-      let :day, do: Timex.to_datetime({1984 , 5, 11}, "Europe/Amsterdam")
+      let :day, do: Timex.to_datetime({1984 , 5, 11}, timezone())
 
       it "rise" do
-        rise = Timex.to_datetime({{1984, 5, 11}, {5, 51, 00}}, "Europe/Amsterdam")
+        rise = Timex.to_datetime({{1984, 5, 11}, {5, 51, 00}}, timezone())
 
         expect(SunTimes.rise(day(), lat(), lon())).to be_close_to(rise, tolerance())
       end
 
       it "set" do
-        set = Timex.to_datetime({{1984, 5, 11}, {21, 23, 00}}, "Europe/Amsterdam")
+        set = Timex.to_datetime({{1984, 5, 11}, {21, 23, 00}}, timezone())
 
         expect(SunTimes.set(day(), lat(), lon())).to be_close_to(set, tolerance())
       end
@@ -62,34 +63,35 @@ defmodule SunTimesSpec do
   describe "Istanbul" do
     let :lat, do: 41.0082
     let :lon, do: 28.9784
+    let :timezone, do: "Europe/Istanbul"
 
     describe "on Jan 14th 2018" do
-      let :day, do: Timex.to_datetime({2018 , 1, 14}, "Europe/Istanbul")
+      let :day, do: Timex.to_datetime({2018 , 1, 14}, timezone())
 
       it "rise" do
-        rise = Timex.to_datetime({{2018, 1, 14}, {8, 27, 00}}, "Europe/Istanbul")
+        rise = Timex.to_datetime({{2018, 1, 14}, {8, 27, 00}}, timezone())
 
         expect(SunTimes.rise(day(), lat(), lon())).to be_close_to(rise, tolerance())
       end
 
       it "set" do
-        set = Timex.to_datetime({{2018, 1, 14}, {17, 59, 00}}, "Europe/Istanbul")
+        set = Timex.to_datetime({{2018, 1, 14}, {17, 59, 00}}, timezone())
 
         expect(SunTimes.set(day(), lat(), lon())).to be_close_to(set, tolerance())
       end
     end
 
     describe "on May 11th 1984" do
-      let :day, do: Timex.to_datetime({1984 , 5, 11}, "Europe/Istanbul")
+      let :day, do: Timex.to_datetime({1984 , 5, 11}, timezone())
 
       it "rise" do
-        rise = Timex.to_datetime({{1984, 5, 11}, {5, 50, 00}}, "Europe/Istanbul")
+        rise = Timex.to_datetime({{1984, 5, 11}, {5, 50, 00}}, timezone())
 
         expect(SunTimes.rise(day(), lat(), lon())).to be_close_to(rise, tolerance())
       end
 
       it "set" do
-        set = Timex.to_datetime({{1984, 5, 11}, {20, 11, 00}}, "Europe/Istanbul")
+        set = Timex.to_datetime({{1984, 5, 11}, {20, 11, 00}}, timezone())
 
         expect(SunTimes.set(day(), lat(), lon())).to be_close_to(set, tolerance())
       end
@@ -99,9 +101,10 @@ defmodule SunTimesSpec do
   describe "Ny-Ålesund" do
     let :lat, do: 78.959137
     let :lon, do: 11.4599269
+    let :timezone, do: "Europe/Amsterdam"
 
     describe "on Jan 1st 2018" do
-      let :day, do: Timex.to_datetime({2018, 1, 1}, "Europe/Amsterdam")
+      let :day, do: Timex.to_datetime({2018, 1, 1}, timezone())
 
       it "rise" do
         expect(SunTimes.rise(day(), lat(), lon())).to eq({:error, "Never rises"})
@@ -113,7 +116,7 @@ defmodule SunTimesSpec do
     end
 
     describe "on Aug 1st 2018" do
-      let :day, do: Timex.to_datetime({2018, 8, 1}, "Europe/Amsterdam")
+      let :day, do: Timex.to_datetime({2018, 8, 1}, timezone())
 
       it "rise" do
         expect(SunTimes.rise(day(), lat(), lon())).to eq({:error, "Never sets"})
